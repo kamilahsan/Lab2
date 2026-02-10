6ac674cb647d0fabe65b81e148445f06540e82c0
Kamil Ahsan <ahsank8483@gmail.com>
2026-02-09 18:46:44 -0800

    hi

---
+++ GetToKnowYou.java
@@ -0,0 +1,13 @@
+
+/**
+ * Name, Major, and Fun Fact on 3 lines using one statement
+ *
+ * @author Kamil Ahsan, kahsan@student.sdccd.edu
+ * @since 2/9/2026
+ * @version v1.0
+ */
+public class GetToKnowYou {
+    public static void main(String[] args) {
+        System.out.println("Kamil Ahsan\nPsychology\nI like to read in my free time");
+    }
+}
